9156797174c67b5daa113b353cbb6fad19846284
Alex Revin <lyssdod@gmail.com>
2016-09-19 16:19:34 +0300

    Fix missing

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-require(['kute.js', 'kute.js/kute-svg'], function(kute) {
-	var multiMorph1 = KUTE.to('#w11', { path: '#w24' }).start();
-	var multiMorph2 = KUTE.to('#w13', { path: '#w21' }).start();
-	var multiMorph3 = KUTE.to('#w14', { path: '#w22' }).start();
-	var multiMorph3 = KUTE.to('#w12', { path: '#w23' }).start();
+require(['kute.js', 'kute.js/kute-svg'], function(K) {
+	var multiMorph1 = K.to('#w11', { path: '#w24' }).start();
+	var multiMorph2 = K.to('#w13', { path: '#w21' }).start();
+	var multiMorph3 = K.to('#w14', { path: '#w22' }).start();
+	var multiMorph3 = K.to('#w12', { path: '#w23' }).start();
 });
+
